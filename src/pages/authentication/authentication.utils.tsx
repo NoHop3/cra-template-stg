9fc6f5b9e4ba560e292ce506e3244825b2ba481b
Stefan Georgiev <u_sux@abv.bg>
2023-09-28 10:14:34 +0200

    build(package.json & template.json): changes to the eslint package
    Removing eslint package as create-react-app uses eslint as peer dep. Adding eslint-config-react-app
    as devDep due to facing run-time error

--- src/pages/authentication/authentication.utils.tsx
+++ src/pages/authentication/authentication.utils.tsx
@@ -4,7 +4,7 @@ export const Copyright = (props: any) => {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://github.com/Automating-Hydroponics-BPR2/via-sem7-bpr2-client">
+      <Link color="inherit" href="https://github.com/NoHop3/cra-template-stg">
         Your copyrights go here
       </Link>{' '}
       {new Date().getFullYear()}
